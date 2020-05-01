8eade24db774377b561cb647434ddb6890e6aa73
martin <mail@martinbaur.eu>
2020-05-01 15:04:13 +0200

    Solution 1a

--- Exercises/ListKeeperModule.py
+++ Exercises/ListKeeperModule.py
@@ -1,151 +1,27 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "kernelspec": {
-      "display_name": "Python 3",
-      "language": "python",
-      "name": "python3"
-    },
-    "language_info": {
-      "codemirror_mode": {
-        "name": "ipython",
-        "version": 3
-      },
-      "file_extension": ".py",
-      "mimetype": "text/x-python",
-      "name": "python",
-      "nbconvert_exporter": "python",
-      "pygments_lexer": "ipython3",
-      "version": "3.7.6"
-    },
-    "colab": {
-      "name": "1_a_Python.ipynb",
-      "provenance": [],
-      "toc_visible": true
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "lqka5QR0CDJ9",
-        "colab_type": "text"
-      },
-      "source": [
-        "# Exercise I: Your own Python module "
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "8VmY-9ruCDKA",
-        "colab_type": "code",
-        "colab": {
-          "base_uri": "https://localhost:8080/",
-          "height": 104
-        },
-        "outputId": "7c306b82-46c1-4b05-c436-bcd1a6e455c4"
-      },
-      "source": [
-        "#check if notebook runs in colab\n",
-        "import sys\n",
-        "IN_COLAB = 'google.colab' in sys.modules\n",
-        "print('running in Colab:',IN_COLAB)\n",
-        "path='..'\n",
-        "if IN_COLAB:\n",
-        "  #in colab, we need to clone the data from the repo\n",
-        "  !git clone https://github.com/mrmarthy/DataScienceSS20.git\n",
-        "  %cd DataScienceSS20/\n",
-        "  !git checkout mrmarthy-solution\n",
-        "  %cd ..\n",
-        "  path='DataScienceSS20'\n",
-        "  import sys\n",
-        "  sys.path.append(path+'/Exercises/')"
-      ],
-      "execution_count": 13,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "text": [
-            "running in Colab: True\n",
-            "fatal: destination path 'DataScienceSS20' already exists and is not an empty directory.\n",
-            "/content/DataScienceSS20/Exercises/DataScienceSS20\n",
-            "error: pathspec 'mrmarthy-solution' did not match any file(s) known to git.\n",
-            "/content/DataScienceSS20/Exercises\n"
-          ],
-          "name": "stdout"
-        }
-      ]
-    },
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "9i8tl3EeCDKQ",
-        "colab_type": "text"
-      },
-      "source": [
-        "Write your own python module (choose a name) with the following properties:\n",
-        "\n",
-        "1. start new python file for the module\n",
-        "2. define a class ***ListKeeper*** with the following properties and API:\n",
-        "   1. ***Listkeeper*** strores named lists (hint: use dictionaries) \n",
-        "   2. it initializes with a list named *example*: [1,2,3,4,5]\n",
-        "   3. ``show()`` returns all list names\n",
-        "   4. ``add(name, list)`` adds a new list\n",
-        "   5. ``delete(name)`` deletes list\n",
-        "   6. ``sort(name)`` returns the sorted list *name*\n",
-        "   7. ``append(name, list)`` appends *list* to *name* \n",
-        "3. add comments and documentation to your class\n",
-        "4. Import your module in this notebook\n",
-        "5. write tests to check the functionality of your class "
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "dd5VeL7WCDKW",
-        "colab_type": "code",
-        "colab": {
-          "base_uri": "https://localhost:8080/",
-          "height": 34
-        },
-        "outputId": "ecd8c1e6-74db-4321-9f79-a822cae86887"
-      },
-      "source": [
-        "import ListKeeperModule\n",
-        "\n",
-        "lk = ListKeeperModule.ListKeeper()\n",
-        "lk.show()"
-      ],
-      "execution_count": 16,
-      "outputs": [
-        {
-          "output_type": "execute_result",
-          "data": {
-            "text/plain": [
-              "['example']"
-            ]
-          },
-          "metadata": {
-            "tags": []
-          },
-          "execution_count": 16
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "Q9IjjzS3D7oZ",
-        "colab_type": "code",
-        "colab": {}
-      },
-      "source": [
-        ""
-      ],
-      "execution_count": 0,
-      "outputs": []
-    }
-  ]
-}
+class ListKeeper:
+  lists = {}
+  def __init__(self):
+    self.lists["example"] = [1,2,3,4,5]
+
+  def show(self):
+    names = []
+    for x in self.lists:
+      names.append(x)
+    return names
+
+  def add(self, name, list):
+    self.lists[name] = list
+
+  def delete(self, name):
+    self.lists.pop(name)
+
+  def sort(self, name):
+    x = self.lists.get(name)
+    x.sort()
+    return x
+
+  def append(self, name, list):
+    for newItem in list:
+      x = self.lists.get(name)
+      x.append(newItem)
+"
